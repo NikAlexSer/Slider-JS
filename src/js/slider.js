0c67afbab982f76cde4f048299b0afd0efd25922
Alexander Nikiforov <alexandernikser@gmail.com>
2016-09-27 15:52:20 +0500

    Remove unnecessary html-tag

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,15 +1,16 @@
 var Slider = function(arraySlides) {
-  var pos = -1, 
-    index,
+  var index,
     totalSlides,
     autoSlider,
     slideWidth,
+    position = 0 ,
     duration = 1000,
     $next,
     $prev,
     $slide,
     $bullets,
     $holder,
+    $slider,
     sourceSlides,
     templateSlides,
     contextSlides,
@@ -31,63 +32,41 @@ var Slider = function(arraySlides) {
     $next = $('.control-next');
     $prev =  $('.control-prev');
     $slide = $('.js-content-holder li');
+    $slider = $('.slider');
     $holder = $('.js-content-holder');
     $bullets = $('.js-nav span');
     $('.js-nav span:first-child').addClass('on');
-    $holder.addClass('animated');
     totalSlides =  $slide.length;
     slideWidth = $('.js-content-holder img').width();
     $holder.css({width: slideWidth * totalSlides});
+    $holder.addClass('animated');
+  };
+
+  function _addEvents() {
+    $next.on('click', (function() {
+      _move(-1);
+    }));
+    $prev.on('click', (function() {
+      _move(1);
+    }));
+    $slider.on('mouseenter', function() {
+      clearInterval(autoSlider);
+    });
+    $slider.on('mouseleave', function() {
+      _start();
+    });
   };
   function _start() {
     autoSlider = setInterval(function () {
-      _move(1);
+      _move(-1);
     }, duration);
   };
   function _move(direction) {
     $('.on').removeClass('on');
-    if (direction === 1) {
-      pos++;
-      if (pos === totalSlides) {
-        pos = 0;
-      }
-      $holder.css({left: -(slideWidth*(pos))});
-      index = parseInt($slide.eq(pos).data('number'));
-      $bullets.eq(index).addClass('on');
-    }
-    else {
-      pos--;
-      if (pos === -1){
-        pos = totalSlides - 1;
-      }
-      $holder.css({left: -(slideWidth*(pos))});
-      index = +$slide.eq(pos).data('number');
-      $bullets.eq(index).addClass('on');
-    }
-  };
-  function _addEvents() {
-    $next.on('click', (function() {
-      _move(1);
-    }));
-    $prev.on('click', (function() {
-      _move();
-    }));
-    $slide
-      .add($prev)
-      .add($next)
-      .add($bullets).on('mouseenter', function() {
-      clearInterval(autoSlider);
-    });
-    $slide
-      .add($prev)
-      .add($next).on('mouseleave', function() {
-      _start();
-    });
-    $bullets.on('click', function() {
-      pos = $(this).index() - 1;
-      clearInterval(autoSlider);
-      _move(1);
-    });
+    position = position + direction * slideWidth;  // -1 right, 1 left
+    $holder.css({'transform':'translateX('+(position)+'px)'});
+    index = parseInt($slide.eq(Math.abs(Math.round(position/slideWidth))).data('number'));
+    $bullets.eq(index).addClass('on');
   };
   this.init = function() {
     _templateInit();
