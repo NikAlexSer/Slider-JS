c7abaf69ed14f7516510e9532aee255c5651edcb
NikAlexSer <alexandernikser@gmail.com>
2016-10-05 20:03:26 +0500

    Clean some shit

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,8 @@
 var Slider = function(data) {
   var
-      _template;
+      _template,
+      _interval,
+      _index = 0;
 
   // Handlebars functions
   function _templateInit() {
@@ -10,8 +12,23 @@ var Slider = function(data) {
     $('.js-slider').html(_template({slidesTemp: arraySlides}));
   };
 
+  function _prepare() {
+    $('.js-content-holder').css({width: $('.js-content-holder img').width() * ($('.js-content-holder li').length + 2)});
+  };
 
+  function _setInterval() {
+    var duration = 1000;
+    _interval = setInterval(function() {
+      _slide()
+    }, duration)
+  };
 
+  function _slide() {
+    $('.js-content-holder').css("transform", "translateX(-" + _index * 500 + "px) ").addClass('animated');
+    _index++;
+    $('li').eq(_index).clone().appendTo($('.js-content-holder'));
+    //$('.animated').removeClass('animated')
+  }
 
   this.init = function() {
     _templateInit();
@@ -19,5 +36,9 @@ var Slider = function(data) {
 
   this.render = function() {
     _templateRender(data);
+    console.log('lol');
+    _prepare();
+    console.log('ziga');
+    _setInterval();
   };
 };
